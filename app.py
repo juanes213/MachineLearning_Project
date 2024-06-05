e072b2937b2a33ebdf622a96669317424193cc43
juanes213 <albisj@uninorte.edu.co>
2024-06-05 14:47:01 -0500

    Update app.py for deployment

--- app.py
+++ app.py
@@ -335,4 +335,4 @@ def update_graph_live(n):
         raise dash.exceptions.PreventUpdate
 
 if __name__ == '__main__':
-    app.run_server(debug = True)
+    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
